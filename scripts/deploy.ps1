2b263b4dce14b6defbab6d4b8e1e8c285a403548
askrivenko <as.krivenko@stalcom.com>
2019-10-13 22:22:20 +0300

    docker push command added

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -11,5 +11,10 @@ $projectDir = $buildDir + "\";
 Write-Host "Build docker image" -ForegroundColor Green
 docker build -t $env:DOCKER_USER/$env:DOCKER_IMAGE_NAME -f Dockerfile .
 
+# Push docker image
+Write-Host "Push docker image" -ForegroundColor Green
+docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
+docker push $env:DOCKER_USER/$env:DOCKER_IMAGE_NAME
+
 # Done
 Write-Host "Done!" -ForegroundColor Green
